e811edd33cc7abee00ec3a09687ab4d954c0b0ec
Cate Nduati <nduaticate001@gmail.com>
2025-05-31 22:46:10 +0300

    Navigation setup

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/layout/Header";
+
+const posts: React.FC = () =>{
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default posts;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/layout/Header";
+
+const users: React.FC = () =>{
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default users;
