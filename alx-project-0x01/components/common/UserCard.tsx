5fec4d0939f9c807ccb7de82472b727f3717fb4a
Cate Nduati <nduaticate001@gmail.com>
2025-06-01 06:00:05 +0300

    UserCard component

---
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -0,0 +1,15 @@
+import { UserProps } from "@/interfaces";
+
+const UserCard: React.FC<UserProps> = ({ name, email, phone, website, company }) => {
+  return (
+    <div className="max-w-md mx-auto my-4 p-6 bg-white rounded-lg shadow-md">
+      <h2 className="text-xl font-semibold">{name}</h2>
+      <p className="text-sm text-gray-500">{email}</p>
+      <p className="text-sm text-gray-500">{phone}</p>
+      <p className="text-sm text-blue-500">{website}</p>
+      <p className="text-sm italic mt-2">{company.name}</p>
+    </div>
+  );
+};
+
+export default UserCard;
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,26 @@
 import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const users: React.FC = () =>{
-    return (
-        <>
-            <Header />
-        </>
-    )
+const Users = ({ posts }: { posts: UserProps[] }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-semibold mb-4">User List</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {posts.map((user, idx) => <UserCard key={idx} {...user} />)}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
+
+  return { props: { posts } };
 }
 
-export default users;
+export default Users;
