33e35db05fb917a13a0ed91e4d5a1653f9131894
Cate Nduati <nduaticate001@gmail.com>
2025-06-01 07:01:17 +0300

    UserModal component

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -10,6 +10,42 @@ export interface UserProps {
   name: string;
   username: string;
   email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
+export interface UserData {
+  id?: number;
+  name: string;
+  username: string;
+  email: string;
   phone: string;
   website: string;
   address: {
@@ -29,14 +65,7 @@ export interface UserProps {
   };
 }
 
-export interface PostData {
-  userId: number;
-  id?: number;
-  title: string;
-  body: string;
-}
-
-export interface PostModalProps {
+export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (post: PostData) => void;
+  onSubmit: (post: UserProps) => void;
 }
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,26 +1,60 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
-import { UserProps } from "@/interfaces";
+import UserModal from "@/components/common/UserModal";
+import { UserProps, UserData } from "@/interfaces";
+import { useState } from "react";
 
-const Users = ({ posts }: { posts: UserProps[] }) => {
+const Users: React.FC<UserProps[]> = ({ users }: any) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [posts, setUserList] = useState<UserProps[]>(users);
+
+  const handleAddUser = (newUser: UserData) => {
+    const newUserWithId: UserProps = {
+      ...newUser,
+      id: posts.length + 1,
+    };
+    setUserList([...posts, newUserWithId]);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <h1 className="text-2xl font-semibold mb-4">User List</h1>
+        <div className="flex justify-between mb-4">
+          <h1 className="text-2xl font-semibold">User List</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add User
+          </button>
+        </div>
         <div className="grid grid-cols-3 gap-4">
-          {posts.map((user, idx) => <UserCard key={idx} {...user} />)}
+          {posts.map((user: UserProps, index: number) => (
+            <UserCard key={index} {...user} />
+          ))}
         </div>
       </main>
+
+      {isModalOpen && (
+        <UserModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddUser}
+        />
+      )}
     </div>
   );
 };
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const users = await response.json();
 
-  return { props: { posts } };
+  return {
+    props: {
+      users,
+    },
+  };
 }
 
 export default Users;
